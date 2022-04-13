0a289381a793c0bd6b808ec3c63b64e29eed5553
Simran <simran.windows@gmail.com>
2022-04-13 14:08:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {  useState } from 'react';
 
 function App() {
+  const [userInput, setSomething] = useState({
+    name:         "",
+    usn:          "",
+    university:   "",
+    gender:       "",
+    phoneNumber:  ""
+  });
+  const [shouldIShowValue, setShouldIShowValue] = useState(false);
+  
+
+
+
+  const handleNameChange = (event) => {
+    const newObject = {
+      name:         event.Target.value,
+      usn:          userInput.usn,
+      university:   userInput.university,
+      gender:       userInput.gender,
+      phoneNumber:  userInput.phoneNumber
+    }
+    setSomething(newObject)
+  }
+
+  const handleDataVisibility = () => {
+    setShouldIShowValue(true);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input 
+          placeholder='Name'
+          onChange = {handleNameChange} 
+          value = {userInput.name}     />
+
+          {
+            shouldIShowValue
+            ?
+            <h1>{userInput.name}</h1>
+            :
+            null
+          }
+
+          <button onClick = {handleDataVisibility} > Show Name</button>
       </header>
     </div>
   );
